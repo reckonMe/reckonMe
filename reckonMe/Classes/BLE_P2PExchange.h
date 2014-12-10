d9afcf11905bf04ac786535673673f5137508331
Benjamin Thiel <ben.thiel23@gmail.com>
2014-12-10 23:28:11 +0100

    add BLE_P2PExchange class

---
+++ reckonMe/Classes/BLE_P2PExchange.h
@@ -0,0 +1,13 @@
+//
+//  BLE_P2PExchange.h
+//  reckonMe
+//
+//  Created by Benjamin Thiel on 10.12.14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BLE_P2PExchange : NSObject
+
+@end
---
+++ reckonMe/Classes/BLE_P2PExchange.m
@@ -0,0 +1,13 @@
+//
+//  BLE_P2PExchange.m
+//  reckonMe
+//
+//  Created by Benjamin Thiel on 10.12.14.
+//
+//
+
+#import "BLE_P2PExchange.h"
+
+@implementation BLE_P2PExchange
+
+@end
