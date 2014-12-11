0b73d54c6d6a8ea6a661edccc0c81a011261feaa
Benjamin Thiel <ben.thiel23@gmail.com>
2014-12-11 09:12:15 +0100

    BLE_P2PExchange: declare methods and properties

--- reckonMe/Classes/BLE_P2PExchange.h
+++ reckonMe/Classes/BLE_P2PExchange.h
@@ -5,9 +5,51 @@
 //  Created by Benjamin Thiel on 10.12.14.
 //
 //
+/**
+ *	The BSD 2-Clause License (aka "FreeBSD License")
+ *
+ *	Copyright (c) 2012, Benjamin Thiel, Kamil Kloch
+ *	All rights reserved.
+ *
+ *	Redistribution and use in source and binary forms, with or without
+ *	modification, are permitted provided that the following conditions are met:
+ *
+ *	1. Redistributions of source code must retain the above copyright notice, this
+ *	   list of conditions and the following disclaimer.
+ *	2. Redistributions in binary form must reproduce the above copyright notice,
+ *	   this list of conditions and the following disclaimer in the documentation
+ *	   and/or other materials provided with the distribution.
+ *
+ *	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *	DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ *	ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *	ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **/
 
 #import <Foundation/Foundation.h>
+#import <CoreBluetooth/CoreBluetooth.h>
+#import "PDRExchange.h"
 
-@interface BLE_P2PExchange : NSObject
+@interface BLE_P2PExchange : NSObject <CBCentralManagerDelegate, CBPeripheralManagerDelegate, CBPeripheralDelegate>
+
+//values above this threshold lead to an exchange
+@property(nonatomic, assign) NSInteger rssiThreshold;
+
+@property (assign) id<PDRControllerProtocol> delegate;
+@property(nonatomic, retain) AbsoluteLocationEntry *advertisedPosition;
+
+//singleton
++ (instancetype)sharedInstance;
+
+-(void)startStationaryBeaconModeAtPosition:(AbsoluteLocationEntry *)position;
+-(void)startWalkerMode;
+
+-(void)stop;
 
 @end
